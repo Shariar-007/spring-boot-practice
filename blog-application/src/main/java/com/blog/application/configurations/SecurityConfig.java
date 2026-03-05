e2e6fa9be495f828a35255f3b895955aca0ba53a
Shariar Shohag <uiu.151007@gmail.com>
2026-03-05 00:08:42 -0500

    Project: blog application =>  checking swgger configuration

--- blog-application/src/main/java/com/blog/application/configurations/SecurityConfig.java
+++ blog-application/src/main/java/com/blog/application/configurations/SecurityConfig.java
@@ -19,9 +19,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @EnableWebSecurity
 @Configuration
+@EnableWebMvc
 @EnableMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
     public static final String[] PUBLIC_URLS = {"/api/v1/auth/login", "/v3/api-docs/**", "/swagger-ui/**","/swagger-ui.html", "/api/v1/auth/register", "/login"};
--- blog-application/src/main/java/com/blog/application/configurations/SwaggerConfig.java
+++ blog-application/src/main/java/com/blog/application/configurations/SwaggerConfig.java
@@ -1,6 +1,7 @@
 package com.blog.application.configurations;
 
 import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
@@ -10,21 +11,46 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
+//    @Bean
+//    public OpenAPI customOpenAPI() {
+//        final String securitySchemeName = "bearerAuth";
+//        return new OpenAPI()
+//                .info(new Info()
+//                        .title("Blog Application API")
+//                        .version("1.0")
+//                        .description("Backend APIs for the Blog Application project developed."))
+//                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+//                .components(new Components()
+//                        .addSecuritySchemes(securitySchemeName,
+//                                new SecurityScheme()
+//                                        .name(securitySchemeName)
+//                                        .type(SecurityScheme.Type.HTTP)
+//                                        .scheme("bearer")
+//                                        .bearerFormat("JWT")));
+//    }
+
     @Bean
     public OpenAPI customOpenAPI() {
-        final String securitySchemeName = "bearerAuth";
+
         return new OpenAPI()
                 .info(new Info()
-                        .title("Blog Application API")
+                        .title("Blogging Application API")
                         .version("1.0")
-                        .description("Backend APIs for the Blog Application project developed."))
-                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                        .description("API documentation for Blogging Application")
+                        .contact(new Contact()
+                                .name("Shahriar")
+                                .email("blog@gmail.com")
+                                .url("https://blogging.com")))
+
+                .addSecurityItem(new SecurityRequirement().addList("BearerAuth"))
+
                 .components(new Components()
-                        .addSecuritySchemes(securitySchemeName,
+                        .addSecuritySchemes("BearerAuth",
                                 new SecurityScheme()
-                                        .name(securitySchemeName)
+                                        .name("Authorization")
                                         .type(SecurityScheme.Type.HTTP)
                                         .scheme("bearer")
                                         .bearerFormat("JWT")));
     }
+
 }
